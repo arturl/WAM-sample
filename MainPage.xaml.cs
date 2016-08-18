63078d8d464162b9f27ea78b201be67dfaca58a9
Artur Laksberg <arturl@microsoft.com>
2016-08-18 12:58:26 -0700

    Code cleanup

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -50,7 +50,7 @@ namespace WAMUWP
         // To obtain azureAD tokens, you must register this app on the AzureAD portal, and obtain the client ID
         const string AzureActiveDirectoryClientId = "5cec90d8-14eb-41ac-9b2c-b3e8a7d95da5";
         const string AzureActiveDirectoryScopeRequested = "wl.basic";
-        const string AzureResourceUri = "https://management.azure.com/";
+
         private const string AzureResourceApi = "2015-01-01";
         public MainPage()
         {
@@ -82,11 +82,12 @@ namespace WAMUWP
         {
             var scope = MicrosoftAccountScopeRequested;
 
-            //scope = "service::ssl.live.com::MBI_SSL";
+            //scope = "service::insiderservice.microsoft.com::MBI_SSL";
+            //scope = "service::wl.basic::DELEGATION";
             scope = "wl.basic";
 
             // ClientID is ignored by MSA
-            WebTokenRequest request = new WebTokenRequest(command.WebAccountProvider, scope, MicrosoftAccountClientId, WebTokenRequestPromptType.Default);
+            WebTokenRequest request = new WebTokenRequest(command.WebAccountProvider, scope, "none");
 
             //var account = await WebAuthenticationCoreManager.FindAccountAsync(command.WebAccountProvider, "77ab17918e49b08f23b5b5c4f5bb01a3");
             WebAccount account = null;
@@ -106,27 +107,33 @@ namespace WAMUWP
         private async Task AuthenticateWithRequestToken(WebTokenRequest request, WebAccount account)
         {
             WebTokenRequestResult result;
-            if (account != null)
+            if (account == null)
             {
-                result = await WebAuthenticationCoreManager.GetTokenSilentlyAsync(request, account);
+                result = await WebAuthenticationCoreManager.RequestTokenAsync(request);
             }
             else
             {
-                result = await WebAuthenticationCoreManager.RequestTokenAsync(request);
+                result = await WebAuthenticationCoreManager.GetTokenSilentlyAsync(request, account);
             }
 
             if (result.ResponseStatus == WebTokenRequestStatus.Success)
             {
-                //WebAccount account = result.ResponseData[0].WebAccount;
-                //await account.SignOutAsync();
+                string userName = result.ResponseData[0].WebAccount.UserName;
 
                 string token = result.ResponseData[0].Token;
                 List<string> subscriptionIds = new List<string>();
                 string relative = "subscriptions?api-version=" + AzureResourceApi;
-                try
+
+                var client = new System.Net.Http.HttpClient();
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var restApi = new Uri("https://management.azure.com/" + relative);
+                var infoResult = await client.GetAsync(restApi);
+                string content = await infoResult.Content.ReadAsStringAsync();
+                var jsonObject = JsonObject.Parse(content);
+                if (jsonObject.ContainsKey("value"))
                 {
-                    JsonObject data = await GetIoTData(relative, token);
-                    var subscriptions = data["value"].GetArray();
+                    var subscriptions = jsonObject["value"].GetArray();
                     subscriptionIds.AddRange(subscriptions.Select(_ => _.GetObject().GetNamedValue("subscriptionId").GetString()));
                     AzureSubscription.Items.Clear();
                     if (subscriptionIds.Count == 0)
@@ -137,20 +144,13 @@ namespace WAMUWP
                     {
                         AzureSubscription.Items.Add(id);
                     }
+
                 }
-                catch (System.Exception e)
+                else
                 {
-                    if (depth == 0)
-                    {
-                        depth++;
-                        await result.InvalidateCacheAsync();
-                        await AuthenticateWithRequestToken(request, account);
-                    }
-                    else
-                    {
-                        Debug.WriteLine(e.Message);
-                        AzureSubscription.Items.Add("ERROR: " + e.Message);
-                    }
+                    string error = jsonObject.GetObject().GetNamedValue("error").GetObject().GetNamedValue("message").ToString();
+                    AzureSubscription.Items.Clear();
+                    AzureSubscription.Items.Add(error);
                 }
             }
             else
@@ -158,29 +158,6 @@ namespace WAMUWP
                 AzureSubscription.Items.Clear();
                 AzureSubscription.Items.Add(result.ResponseStatus.ToString());
             }
-        }
-        private async Task<JsonObject> GetIoTData(string relative, string token)
-        {
-
-            using (var client = new System.Net.Http.HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                var restApi = new Uri(AzureResourceUri + relative);
-                var infoResult = await client.GetAsync(restApi);
-                string content = await infoResult.Content.ReadAsStringAsync();
-                var jsonObject = JsonObject.Parse(content);
-                if (jsonObject.ContainsKey("value"))
-                {
-                    return jsonObject;
-                }
-                else
-                {
-                    string error = jsonObject.GetObject().GetNamedValue("error").GetObject().GetNamedValue("message").ToString();
-                    throw new System.Exception(error);
-                }
-            }
-
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
